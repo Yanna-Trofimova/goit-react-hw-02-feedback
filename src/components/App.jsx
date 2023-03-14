f98ab975bebec2906c62be765c705d58c0a281a2
Yanna-Trofimova <111576903+Yanna-Trofimova@users.noreply.github.com>
2023-03-14 23:58:55 +0200

    s

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,66 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+
+
+import React, { Component } from 'react';
+import Section from 'components/Section/Section';
+import Statistics from 'components/Statistics/Statistics';
+import FeedbackOptions from 'components/FeedbackOptions/FeedbackOptions';
+import Notification from 'components/Notification/Notification';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  hadleClick = e => {
+    const { name } = e.target;
+    this.setState(prevState => ({
+      [name]: prevState[name] + 1,
+    }));
+  };
+
+  countSumOfFeedbacks() {
+    return Object.values(this.state).reduce((acc, currentValue) => {
+      return acc + currentValue;
+    }, 0);
+  }
+
+  countPositiveFeedback() {
+    const { good } = this.state;
+    return Math.round((good / this.countSumOfFeedbacks()) * 100);
+  }
+
+  render() {
+    const { good, bad, neutral } = this.state;
+
+    return (
+      <>
+       
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={Object.keys(this.state)}
+            onBtnClick={this.hadleClick}
+          />
+        </Section>
+
+        <Section title="Statistics">
+          {good === 0 && bad === 0 && neutral === 0 ? (
+            <Notification message="There is no feedback" />
+          ) : (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              sum={this.countSumOfFeedbacks()}
+              positiveFeedback={this.countPositiveFeedback()}
+            />
+          )}
+          </Section>
+          
+      </>
+    );
+  }
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+import  App  from 'components/App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App hello />
+    <App
+    style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101'
+      }}/>
   </React.StrictMode>
 );
